060e950072b9e1b80e2a5031435f6cb1a003d46f
BBlack <675040625@qq.com>
2019-07-01 20:08:54 +0800

    配置muduo

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -x
 
 SOURCE_DIR=`pwd`
-BUILD_DIR=${BUILD_DIR:-../build}
+BUILD_DIR=${BUILD_DIR:-build}
 BUILD_TYPE=${BUILD_TYPE:-release}
 INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install-cpp11}
 CXX=${CXX:-g++}
@@ -17,7 +17,7 @@ mkdir -p $BUILD_DIR/$BUILD_TYPE-cpp11 \
            -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
            -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
            $SOURCE_DIR \
-  && make $*
+  && make -j8 $*
 
 # Use the following command to run all the unit tests
 # at the dir $BUILD_DIR/$BUILD_TYPE :
