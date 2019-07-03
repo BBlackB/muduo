41f585b6cec71d4cd9eb1c212f1a3357e4d4a514
BBlack <675040625@qq.com>
2019-07-03 21:06:03 +0800

    完成muduo和NDSL基类的线程池(CPU密集型)测试

--- tests/threadpool_testing_pressure.cc
+++ tests/threadpool_testing_pressure.cc
@@ -5,68 +5,34 @@
 #include <stdio.h>
 #include <ctime>
 
-// #define s_rand() double(1.0 * rand() / RAND_MAX)
+#define s_rand() double(1.0 * rand() / RAND_MAX)
 
-// double MontePI(int n)
-// {
-//   double PI;
-//   double x, y;
-//   int i = 0, sum = 0;
-//   srand((int)time(0));
-//   for (i = 0; i < n; i++)
-//   {
-//     x = s_rand();
-//     y = s_rand();
-//     if (x * x + y * y < 1)
-//     {
-//       sum++;
-//     }
-//   }
-//   PI = 4.0 * sum / n;
-//   return PI;
-// }
-
-// double arctan(double x)
-// {
-
-//   double sqr = x * x;
-
-//   double e = x;
-
-//   double r = 0;
-
-//   int i = 1;
-
-//   while (e / i > 1e-15)
-//   {
-
-//     double f = e / i;
-
-//     r = (i % 4 == 1) ? r + f : r - f;
-
-//     e = e * sqr;
-
-//     i += 2;
-//   }
-
-//   return r;
-// }
-
-void print()
+double MontePI(int n)
 {
-  // int n = 10;
-  // while (n--)
-  // {
-  //   double a = 16.0 * arctan(1 / 5.0);
-  //   double b = 4.0 * arctan(1 / 239.0);
-  //   double c = a - b;
-  //   c--;
-  // }
-  // double pi = MontePI(5000);
-  // int n = 20000;
-  // while (n-- > 0)
-  //   ;
-  // usleep(1);
+  double PI;
+  double x, y;
+  int i = 0, sum = 0;
+  srand(static_cast<unsigned>(time(NULL)));
+  for (i = 0; i < n; i++)
+  {
+    x = s_rand();
+    y = s_rand();
+    if (x * x + y * y < 1)
+    {
+      sum++;
+    }
+  }
+  PI = 4.0 * sum / n;
+  return PI;
+}
+
+void print(int n)
+{
+  // double pi = MontePI(100000);
+  // printf("pi = %lf\n", pi);
+  // (void)pi;
+
+  // printf("hello.\n");
 }
 
 int main(int argc, char const *argv[])
@@ -75,16 +41,14 @@ int main(int argc, char const *argv[])
   muduo::ThreadPool pool("ThreadPool");
 
   pool.setMaxQueueSize(100);
-  pool.start(8);
+  pool.start(2);
 
   muduo::Timestamp begin(muduo::Timestamp::now());
   int n = 1000000;
   while (n--)
   {
-    pool.run(print);
+    pool.run(std::bind(print, n));
   }
-
-  // printf("done.\n");
 
   muduo::CountDownLatch latch(1);
   pool.run(std ::bind(&muduo::CountDownLatch::countDown, &latch));
