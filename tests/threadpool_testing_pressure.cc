446be6d1c17adbf2ff66cdf3d7e2cc60aed22b6c
BBlack <675040625@qq.com>
2019-07-04 10:26:52 +0800

    为线程池测试 增加命令行参数

--- tests/threadpool_testing_pressure.cc
+++ tests/threadpool_testing_pressure.cc
@@ -1,24 +1,21 @@
-#include "muduo/base/ThreadPool.h"
 #include "muduo/base/CountDownLatch.h"
+#include "muduo/base/ThreadPool.h"
 #include "muduo/base/Timestamp.h"
 
-#include <stdio.h>
 #include <ctime>
+#include <stdio.h>
 
 #define s_rand() double(1.0 * rand() / RAND_MAX)
 
-double MontePI(int n)
-{
+double MontePI(int n) {
   double PI;
   double x, y;
   int i = 0, sum = 0;
   srand(static_cast<unsigned>(time(NULL)));
-  for (i = 0; i < n; i++)
-  {
+  for (i = 0; i < n; i++) {
     x = s_rand();
     y = s_rand();
-    if (x * x + y * y < 1)
-    {
+    if (x * x + y * y < 1) {
       sum++;
     }
   }
@@ -26,27 +23,28 @@ double MontePI(int n)
   return PI;
 }
 
-void print(int n)
-{
-  // double pi = MontePI(100000);
+void print(int n) {
+  double pi = MontePI(100000);
   // printf("pi = %lf\n", pi);
-  // (void)pi;
+  (void)pi;
 
   // printf("hello.\n");
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
+  if (argc != 2) {
+    printf("Usage: %s <threads>\n", argv[0]);
+    return -1;
+  }
 
   muduo::ThreadPool pool("ThreadPool");
 
   pool.setMaxQueueSize(100);
-  pool.start(2);
+  pool.start(atoi(argv[1]));
 
   muduo::Timestamp begin(muduo::Timestamp::now());
-  int n = 1000000;
-  while (n--)
-  {
+  int n = 1000;
+  while (n--) {
     pool.run(std::bind(print, n));
   }
 
